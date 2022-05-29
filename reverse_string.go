d0493c37374454f5ddadb30324839dbbd359ef85
kramal <kramal888@gmail.com>
2022-05-29 19:42:55 +0200

    reverse added edge cases

--- reverse_string.go
+++ reverse_string.go
@@ -2,6 +2,15 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	strLen := len(input)
+
+	if strLen == 0 {
+		return ""
+	}
+
+	if strLen == 1 {
+		return input
+	}
+
 	bstr := []rune(input)
 	for i := 0; i < strLen/2; i++ {
 		bstr[i], bstr[strLen-1-i] = bstr[strLen-1-i], bstr[i]
