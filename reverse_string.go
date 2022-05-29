ba6901b670cee827c29f07971c9789bcec4ff7b2
kramal <kramal888@gmail.com>
2022-05-29 19:41:57 +0200

    reverse

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	strLen := len(input)
+	bstr := []rune(input)
+	for i := 0; i < strLen/2; i++ {
+		bstr[i], bstr[strLen-1-i] = bstr[strLen-1-i], bstr[i]
+	}
+	output = string(bstr)
 	return output
 }
